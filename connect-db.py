def25a465ac818e99197cd8a7877c1cc0ee4b0c5
shmft <lutcenko0710@gmail.com>
2024-05-07 18:45:41 +0300

    Перенес на PostgreSQL и добавил простые стили.

--- connect-db.py
+++ connect-db.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request
-import sqlite3
+import psycopg2
 import getjson
 
-DATABASE = '/blog.db'
 app = Flask(__name__)
 
 
@@ -16,21 +15,29 @@ def index():
 def search():
     """ Обработка запроса и вывод результата """
     if request.method == 'POST':
-        search_query = request.form['search_query']
+        search_query = request.form['search_query']  # Получение данных из input поля
         if len(search_query) >= 3:  # Проверка на длинну полученных данных
-            connection = sqlite3.connect('blog.db')
-            cursor = connection.cursor()
+            connection = psycopg2.connect(  # Соединение с БД
+                host='127.0.0.1',
+                user='postgres',
+                password='root',
+                database='post_db'
+            )
+            cursor = connection.cursor()  # Установка курсора на нашу БД
 
             cursor.execute("""
-                    SELECT Comments.*, Posts.title 
+                    SELECT Comments.body, Posts.title 
                     FROM Comments 
-                    JOIN Posts ON Comments.post_id = Posts.post_id 
-                    WHERE Comments.body LIKE ?
+                    JOIN Posts ON Comments.post_id = Posts.id 
+                    WHERE Comments.body LIKE %s
                 """, ('%' + search_query + '%',))
+            # #SQL
+            # Выбор таблицы с коментариями и обращаемся к заголовкам постов.
+            # Присоединяем данные из таблицы Posts.
+            # Выбор записей, где содержание комментария содержит строку заданную переменной search_query.
 
-            search_results = cursor.fetchall()
-
-            connection.close()
+            search_results = cursor.fetchall()  # Возврат результата
+            connection.close()  # Закрытие БД
 
             if search_results:
                 return render_template('search_results.html', search_results=search_results)
@@ -40,7 +47,6 @@ def search():
             return 'Поисковый запрос должен содержать как минимум 3 символа'
     else:
         render_template('index.html')
-
 
 if __name__ == '__main__':
     getjson.get_json.get_json()
--- getjson.py
+++ getjson.py
@@ -1,5 +1,5 @@
 import requests
-import sqlite3
+import psycopg2
 
 
 class GetJSON:
@@ -20,52 +20,84 @@ class GetJSON:
         self.list_posts = response_posts.json()
         self.list_comments = response_comments.json()
 
+
+
     def create_db(self):
         """ Соединение с базой данных """
-        connection = sqlite3.connect('blog.db')
+        connection = psycopg2.connect(
+            host='127.0.0.1',
+            user='postgres',
+            password='root',
+            database='post_db'
+        )
         self.cur = connection.cursor()
 
         self.cur.execute('''
-        CREATE TABLE IF NOT EXISTS Posts (
-        post_id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL,
-        body TEXT NOT NULL,
-        user_id INTEGER NOT NULL)
+            CREATE TABLE IF NOT EXISTS Posts (
+            id INTEGER PRIMARY KEY,
+            title TEXT NOT NULL,
+            body TEXT NOT NULL,
+            user_id INTEGER NOT NULL);
         ''')
         # Создание таблицы для записей блога
 
         self.cur.execute('''
-        CREATE TABLE IF NOT EXISTS Comments (
-        comment_id INTEGER PRIMARY KEY,
-        post_id INTEGER NOT NULL,
-        name TEXT NOT NULL,
-        email TEXT NOT NULL,
-        body TEXT NOT NULL
-        )
+            CREATE TABLE IF NOT EXISTS Comments (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            body TEXT NOT NULL
+        );
         ''')
         # Создание таблицы для комментариев к записям блога
 
+        self.cur.execute('''
+            SELECT EXISTS (
+            SELECT 1
+            FROM information_schema.columns
+            WHERE table_name = 'Comments' AND column_name = 'post_id'
+            );
+        ''')
+        self.cur.execute('''
+            SELECT post_id 
+            FROM Comments
+        ;
+        ''')
+        if not self.cur.fetchall():
+            self.cur.execute('''
+                ALTER TABLE Comments ADD post_id BIGINT REFERENCES Posts (id);
+            ''')
+        # Создание внешнего ключа
+
         x = y = 0
+
         for post in self.list_posts:
             try:
-                self.cur.execute("INSERT INTO Posts (post_id, title, body, user_id) VALUES (?, ?, ?, ?)",
+                self.cur.execute('''INSERT INTO Posts (id, title, body, user_id) VALUES (%s, %s, %s, %s)
+                                    ON CONFLICT (id) DO UPDATE
+                                    SET title = EXCLUDED.title,
+                                        body = EXCLUDED.body,
+                                        user_id = EXCLUDED.user_id;
+                                    ''',
                                  (post['id'], post['title'], post['body'], post['userId']))
                 x += 1
-            except sqlite3.IntegrityError:
-                self.cur.execute("UPDATE Posts SET title=?, body=?, user_id=? WHERE post_id=?",
-                                 (post['title'], post['body'], post['userId'], post['id']))
-                x += 1
+            except Exception as e:
+                print(f"Error updating post {post['id']}: {e}")
         # Заполнение таблицы блогов
 
         for comment in self.list_comments:
             try:
-                self.cur.execute("INSERT INTO Comments (comment_id, post_id, name, email, body) VALUES (?, ?, ?, ?, ?)",
+                self.cur.execute('''INSERT INTO Comments (id, post_id, name, email, body) VALUES (%s, %s, %s, %s, %s)
+                                    ON CONFLICT (id) DO UPDATE
+                                    SET post_id = EXCLUDED.post_id,
+                                    name = EXCLUDED.name,
+                                    email = EXCLUDED.email,
+                                    body = EXCLUDED.body;
+                                ''',
                                  (comment['id'], comment['postId'], comment['name'], comment['email'], comment['body']))
                 y += 1
-            except sqlite3.IntegrityError:
-                self.cur.execute("UPDATE Comments SET post_id=?, name=?, email=?, body=? WHERE comment_id=?",
-                                 (comment['postId'], comment['name'], comment['email'], comment['body'], comment['id']))
-                y += 1
+            except Exception as e:
+                print(f"Error updating comment {post['id']}: {e}")
         # Заполнение таблицы комментариев
 
         connection.commit()
